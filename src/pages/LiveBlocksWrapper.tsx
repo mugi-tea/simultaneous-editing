8ad1ca279cd0c7889cbf1a80449cdd6b53ad7fbe
mugi-tea <moriyuujp@gmail.com>
2024-04-27 13:29:10 +0900

    fix lint

--- src/pages/LiveBlocksWrapper.tsx
+++ src/pages/LiveBlocksWrapper.tsx
@@ -1,5 +1,6 @@
 import { RoomProvider } from "../../liveblocks.config";
 import { ClientSideSuspense } from "@liveblocks/react";
+import { Presence } from "../../liveblocks.config";
 import React from "react";
 
 type Props = {
@@ -7,8 +8,13 @@ type Props = {
 };
 
 export const LiveBlocksWrapper = ({ children }: Props) => {
+  const getInitialPresence = (roomId: string): Presence => {
+    // roomIdに対応する初期Presenceを返す
+    return {} as Presence; // 適切な初期値を返す
+  };
+
   return (
-    <RoomProvider id="my-room" initialPresence={{}}>
+    <RoomProvider id="my-room" initialPresence={getInitialPresence}>
       <ClientSideSuspense fallback="Loading…">
         {() => children}
       </ClientSideSuspense>
